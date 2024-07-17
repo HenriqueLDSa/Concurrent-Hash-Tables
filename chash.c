f6d44d96410e3e9c24a5ff41af10eb9de9696739
henriquesa <115197689+HenriqueLDSa@users.noreply.github.com>
2024-07-17 14:05:25 -0400

    .

--- chash.c
+++ chash.c
@@ -2,12 +2,28 @@
 #include <stdint.h>
 #include <string.h>
 
+uint32_t jenkins_one_at_a_time_hash(const uint8_t* key, size_t length);
+void insert(char* key_name, uint32_t salary);
+void delete(char* key_name);
+uint32_t search(char* key_name);
+
 typedef struct hash_struct {
     uint32_t hash;
     char name[50];
     uint32_t salary;
     struct hash_struct* next;
 } hashRecord;
+
+int main() {
+
+    // FILE* commands = fopen("commands.txt", "r");
+
+    // char buffer[] = fscanf(commands, "%s %s,%s", buffer);
+
+    return 0;
+}
+
+
 
 uint32_t jenkins_one_at_a_time_hash(const uint8_t* key, size_t length) {
     size_t i = 0;
@@ -28,7 +44,7 @@ uint32_t jenkins_one_at_a_time_hash(const uint8_t* key, size_t length) {
 }
 
 //inserts a new key-value pair node or updates an existing one
-void insert(char key_name[], uint32_t salary) {
+void insert(char* key_name, uint32_t salary) {
     //compute the hash value of the key
     uint32_t hash = jenkins_one_at_a_time_hash((const uint8_t*)key_name, sizeof(key_name) - 1);
 
@@ -40,9 +56,9 @@ void insert(char key_name[], uint32_t salary) {
 }
 
 //deletes key-value pair node if it exists
-void delete(char key_name[]) {
+void delete(char* key_name) {
     //compute the hash value of the key
-    uint32_t hash = jenkins_one_at_a_time_hash(key_name, sizeof(key_name) - 1);
+    uint32_t hash = jenkins_one_at_a_time_hash((const uint8_t*)key_name, sizeof(key_name) - 1);
 
     //acquire the writer-lock
 
@@ -55,9 +71,9 @@ void delete(char key_name[]) {
 
 //searches for key-value pair node and if found, returns the value; if not found, returns NULL
 //if found, the caller prints the record; otherwise, the caller prints "No Record Found"
-uint32_t search(char key_name[]) {
+uint32_t search(char* key_name) {
     //compute the hash value of the key
-    uint32_t hash = jenkins_one_at_a_time_hash(key_name, sizeof(key_name) - 1);
+    uint32_t hash = jenkins_one_at_a_time_hash((const uint8_t*)key_name, sizeof(key_name) - 1);
 
     // acquire reader-lock
 
